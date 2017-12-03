7f4466d34d46771f3496fb176fa30da770fb9e1d
= <crowesoft@gmail.com>
2017-12-03 13:15:54 +0000

    AoC2017 - Day 3 - Part 2

--- Day3/AdventOfCode2017Day3/AdventOfCode2017Day3/Program.cs
+++ Day3/AdventOfCode2017Day3/AdventOfCode2017Day3/Program.cs
@@ -7,13 +7,58 @@ using System.Threading.Tasks;
 namespace AdventOfCode2017Day3
 {
     using System.Drawing;
+    using System.Net;
 
     class Program
     {
         static void Main(string[] args)
         {
-            var point = Coordinate(289326);
-            var test = Distance(0, 0, point.X, point.Y);
+            var test = ValueLargerThanInput(289326);
+        }
+        
+        static int ValueLargerThanInput(int input)
+        {
+            var data = new List<Tuple<int, int, int>>();
+            
+            var x = 0;
+            var y = 0;
+            var dx = 0;
+            var dy = -1;
+
+            for (var i = 0; i < input; i++)
+            {
+                var sum = SumOfAdjacent(ref data, x, y);
+
+                if (sum > input) return sum;
+
+                data.Add(new Tuple<int, int, int>(x, y, sum));
+                
+                if ((x == y) || ((x < 0) && (x == -y)) || ((x > 0) && (x == 1 - y)))
+                {
+                    var z = dx;
+                    dx = -dy;
+                    dy = z;
+                }
+                x += dx;
+                y += dy;
+            }
+
+            return 0;
+        }
+        
+        static int SumOfAdjacent(ref List<Tuple<int, int, int>> data, int x, int y)
+        {
+            var result = data.FirstOrDefault(p => p.Item1 == x - 1 && p.Item2 == y - 1)?.Item3 ?? 0;
+            result += data.FirstOrDefault(p => p.Item1 == x && p.Item2 == y - 1)?.Item3 ?? 0;
+            result += data.FirstOrDefault(p => p.Item1 == x + 1 && p.Item2 == y - 1)?.Item3 ?? 0;
+            result += data.FirstOrDefault(p => p.Item1 == x - 1 && p.Item2 == y)?.Item3 ?? 0;
+            result += data.FirstOrDefault(p => p.Item1 == x + 1 && p.Item2 == y)?.Item3 ?? 0;
+    
+            result += data.FirstOrDefault(p => p.Item1 == x - 1 && p.Item2 == y + 1)?.Item3 ?? 0;
+            result += data.FirstOrDefault(p => p.Item1 == x && p.Item2 == y + 1)?.Item3 ?? 0;
+            result += data.FirstOrDefault(p => p.Item1 == x + 1 && p.Item2 == y + 1)?.Item3 ?? 0;
+
+            return result == 0 ? 1 : result;
         }
 
         static int Distance(int x1, int y1, int x2, int y2)
@@ -29,7 +74,7 @@ namespace AdventOfCode2017Day3
             var y = 0;
             var dx = 0;
             var dy = -1;
-
+            
             for (var i = 0; i < input; i++)
             {
                 result.X = x;
