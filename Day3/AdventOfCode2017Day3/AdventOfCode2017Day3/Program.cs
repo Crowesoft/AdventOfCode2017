fbe243e6574c68e09b199a0bc45207ddee9579d9
= <crowesoft@gmail.com>
2017-12-03 13:24:27 +0000

    AoC2017 - Day 3 - Part 2, initial tidy up before maths rework

--- Day3/AdventOfCode2017Day3/AdventOfCode2017Day3/Program.cs
+++ Day3/AdventOfCode2017Day3/AdventOfCode2017Day3/Program.cs
@@ -6,8 +6,10 @@ using System.Threading.Tasks;
 
 namespace AdventOfCode2017Day3
 {
+    using System.CodeDom;
     using System.Drawing;
     using System.Net;
+    using System.Runtime.Remoting.Metadata.W3cXsd2001;
 
     class Program
     {
@@ -18,45 +20,42 @@ namespace AdventOfCode2017Day3
         
         static int ValueLargerThanInput(int input)
         {
-            var data = new List<Tuple<int, int, int>>();
+            var data = new List<Tuple<Point, int>>();
+            var point = new Point(0, 0);
+            var dir = new Point(0, -1);
             
-            var x = 0;
-            var y = 0;
-            var dx = 0;
-            var dy = -1;
-
             for (var i = 0; i < input; i++)
             {
-                var sum = SumOfAdjacent(ref data, x, y);
+                var sum = SumOfAdjacent(ref data, point.X, point.Y);
 
                 if (sum > input) return sum;
 
-                data.Add(new Tuple<int, int, int>(x, y, sum));
+                data.Add(new Tuple<Point, int>(point, sum));
                 
-                if ((x == y) || ((x < 0) && (x == -y)) || ((x > 0) && (x == 1 - y)))
+                if ((point.X == point.Y) || ((point.X < 0) && (point.X == -point.Y)) || ((point.X > 0) && (point.X == 1 - point.Y)))
                 {
-                    var z = dx;
-                    dx = -dy;
-                    dy = z;
+                    var z = dir.X;
+                    dir.X = -dir.Y;
+                    dir.Y = z;
                 }
-                x += dx;
-                y += dy;
+
+                point.X += dir.X;
+                point.Y += dir.Y;
             }
 
             return 0;
         }
         
-        static int SumOfAdjacent(ref List<Tuple<int, int, int>> data, int x, int y)
+        static int SumOfAdjacent(ref List<Tuple<Point, int>> data, int x, int y)
         {
-            var result = data.FirstOrDefault(p => p.Item1 == x - 1 && p.Item2 == y - 1)?.Item3 ?? 0;
-            result += data.FirstOrDefault(p => p.Item1 == x && p.Item2 == y - 1)?.Item3 ?? 0;
-            result += data.FirstOrDefault(p => p.Item1 == x + 1 && p.Item2 == y - 1)?.Item3 ?? 0;
-            result += data.FirstOrDefault(p => p.Item1 == x - 1 && p.Item2 == y)?.Item3 ?? 0;
-            result += data.FirstOrDefault(p => p.Item1 == x + 1 && p.Item2 == y)?.Item3 ?? 0;
-    
-            result += data.FirstOrDefault(p => p.Item1 == x - 1 && p.Item2 == y + 1)?.Item3 ?? 0;
-            result += data.FirstOrDefault(p => p.Item1 == x && p.Item2 == y + 1)?.Item3 ?? 0;
-            result += data.FirstOrDefault(p => p.Item1 == x + 1 && p.Item2 == y + 1)?.Item3 ?? 0;
+            var result = data.FirstOrDefault(p => p.Item1.X == x - 1 && p.Item1.Y == y - 1)?.Item2 ?? 0;
+            result += data.FirstOrDefault(p => p.Item1.X == x && p.Item1.Y == y - 1)?.Item2 ?? 0;
+            result += data.FirstOrDefault(p => p.Item1.X == x + 1 && p.Item1.Y == y - 1)?.Item2 ?? 0;
+            result += data.FirstOrDefault(p => p.Item1.X == x - 1 && p.Item1.Y == y)?.Item2 ?? 0;
+            result += data.FirstOrDefault(p => p.Item1.X == x + 1 && p.Item1.Y == y)?.Item2 ?? 0;
+            result += data.FirstOrDefault(p => p.Item1.X == x - 1 && p.Item1.Y == y + 1)?.Item2 ?? 0;
+            result += data.FirstOrDefault(p => p.Item1.X == x && p.Item1.Y == y + 1)?.Item2 ?? 0;
+            result += data.FirstOrDefault(p => p.Item1.X == x + 1 && p.Item1.Y == y + 1)?.Item2 ?? 0;
 
             return result == 0 ? 1 : result;
         }
