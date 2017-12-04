ac5da87ac33a0f8ad2f7074f98ee2cf8083d29d7
= <crowesoft@gmail.com>
2017-12-04 08:42:58 +0000

    AoC2017 - Day 1 - Part 2

--- Day4/AdventOfCode2017Day4/AdventOfCode2017Day4/Program.cs
+++ Day4/AdventOfCode2017Day4/AdventOfCode2017Day4/Program.cs
@@ -11,7 +11,7 @@ namespace AdventOfCode2017Day4
     {
         static void Main(string[] args)
         {
-            var count = File.ReadAllText("c:\\temp\\input.txt").Split(new string[] { "\r\n" }, StringSplitOptions.None).Select(line => line.Split(' ')).Select(words => words.Length == words.Distinct().Count() ? 1 : 0).Sum();
+            var count = (from line in File.ReadAllText("c:\\temp\\input.txt").Split(new string[] { "\r\n" }, StringSplitOptions.None) select line.Split(' ') into words select words.Select(word => string.Concat(word.OrderBy(x => x))).ToList() into sorted select sorted.Count == sorted.Distinct().Count() ? 1 : 0).Sum();
         }
     }
 }
