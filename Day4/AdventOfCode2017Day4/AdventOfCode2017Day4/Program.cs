989628971d9735a54367e6bd108a6fb113be56ab
= <crowesoft@gmail.com>
2017-12-04 08:35:34 +0000

    AoC2017 - Day 1 - Part 1

---
+++ Day4/AdventOfCode2017Day4/AdventOfCode2017Day4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2017Day4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var count = File.ReadAllText("c:\\temp\\input.txt").Split(new string[] { "\r\n" }, StringSplitOptions.None).Select(line => line.Split(' ')).Select(words => words.Length == words.Distinct().Count() ? 1 : 0).Sum();
+        }
+    }
+}
