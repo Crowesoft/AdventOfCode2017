b6751e59789104209515af521a0d8f3bc6ca12d3
= <crowesoft@gmail.com>
2017-12-05 05:44:46 +0000

    AoC2017 - Day 5 - Part 1

---
+++ Day5/AdventOfCode2017Day5/AdventOfCode2017Day5/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2017Day5
+{
+    using System.IO;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = File.ReadAllText("c:\\temp\\input.txt");
+            //var input = "0\r\n3\r\n0\r\n1\r\n-3";
+            var instructions = input.Split(new string[] { "\n" }, StringSplitOptions.None);
+            var pointer = 0;
+            var count = 0;
+            
+            while (pointer >= 0 && pointer < instructions.Length && instructions[pointer] != string.Empty)
+            {
+                var currentValue = int.Parse(instructions[pointer]);
+                var currentPointer = pointer;
+                pointer = pointer + currentValue;
+
+                currentValue++;
+
+                instructions[currentPointer] = currentValue.ToString();
+                count++;
+            }
+            
+        }
+        
+    }
+}
