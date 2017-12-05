5ab44334b8db21dd94427559742e87e1f646fccd
= <crowesoft@gmail.com>
2017-12-05 06:10:03 +0000

    AoC2017 - Day 5 - Part 2 - Code tidy up

--- Day5/AdventOfCode2017Day5/AdventOfCode2017Day5/Program.cs
+++ Day5/AdventOfCode2017Day5/AdventOfCode2017Day5/Program.cs
@@ -1,36 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace AdventOfCode2017Day5
+﻿namespace AdventOfCode2017Day5
 {
+    using System;
+    using System.Linq;
     using System.IO;
 
     class Program
     {
         static void Main(string[] args)
         {
-            var input = File.ReadAllText("c:\\temp\\input.txt");
-            //var input = "0\r\n3\r\n0\r\n1\r\n-3";
-            var instructions = input.Split(new string[] { "\n" }, StringSplitOptions.None);
-            var pointer = 0;
-            var count = 0;
-            
-            while (pointer >= 0 && pointer < instructions.Length && instructions[pointer] != string.Empty)
-            {
-                var currentValue = int.Parse(instructions[pointer]);
-                var currentPointer = pointer;
-                pointer = pointer + currentValue;
+            //var input = File.ReadAllText("c:\\temp\\input.txt");
+            var input = "0\r\n3\r\n0\r\n1\r\n-3";
 
-                currentValue = currentValue >= 3 ? currentValue - 1 : currentValue + 1;
-
-                instructions[currentPointer] = currentValue.ToString();
-                count++;
-            }
-            
+            var test = Compute(input);
         }
-        
+
+        static int Compute(string input)
+        {
+            var instructions = input.Split(new string[] { "\n" }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            var pointer = 0;
+            var jmps = 0;
+
+            while (pointer >= 0 && pointer < instructions.Length)
+            {
+                var value = instructions[pointer];
+                
+                instructions[pointer] = value >= 3 ? value - 1 : value + 1;
+                pointer += value;
+                jmps++;
+            }
+
+            return jmps;
+        }        
     }
 }
